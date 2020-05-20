c0321ed5e78bf4bd34cf7a33da5b62569b988d9a
Naresh Nirala <naresh.mehta8800@gmail.com>
2020-05-21 01:53:27 +0530

    delete

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,67 +1,7 @@
 import React, { Component } from "react";
-import Persons from "../components/Persons/Persons";
-import Cockpit from "../components/Cockpit/Cockpit";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log("App js Constructor");
-
-    this.state = {
-      persons: [
-        { id: "1", name: "Naresh", age: 23 },
-        { id: "1", name: "Naresh", age: 24 },
-        { id: "1", name: "Naresh", age: 25 },
-      ],
-      isShowPerson: false,
-    };
-    // this.deleteHandler = this.deleteHandler.bind(this);
-  }
-  static getDerivedStateFromProps(props, state) {
-    console.log("App js getDerivedSateFromProps", props, state);
-    return state;
-  }
-  tooglePersonHandler = () => {
-    this.setState({ isShowPerson: !this.state.isShowPerson });
-  };
-  clickHandler = (index) => {
-    const persons = [...this.state.persons];
-    persons.splice(index, 1);
-    this.setState({ persons: persons });
-  };
-  deleteHandler = (event, index) => {
-    const persons = [...this.state.persons];
-    const person = { ...persons[index] };
-    person.name = event.target.value;
-    persons[index] = person;
-    this.setState({ persons: persons });
-  };
-  componentWillMount() {
-    console.log("will mount");
-  }
-  componentDidMount() {
-    console.log("app js component did mount");
-  }
   render() {
-    console.log("App js Render");
-    let persons = null;
-    if (this.state.isShowPerson) {
-      persons = (
-        <div>
-          <Persons
-            clicked={this.clickHandler}
-            persons={this.state.persons}
-            changed={this.deleteHandler}
-          />
-        </div>
-      );
-    }
-
-    return (
-      <div>
-        <Cockpit clicked={this.tooglePersonHandler} />
-        {persons}
-      </div>
-    );
+    return <div>Hello</div>;
   }
 }
--- src/faltu/Validation/Validation.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const validation = (props) => {
-  return (
-    <div>
-      {props.textLength > 5 ? <p>Text Too Enough</p> : <p>Text too short</p>}
-    </div>
-  );
-};
-export default validation;
